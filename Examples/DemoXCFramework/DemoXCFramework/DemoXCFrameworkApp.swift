9f30eaf41158c1d6cc430719a77dedf79716a6b7
Petr Palata <petr.palata@gmail.com>
2022-08-03 00:33:02 +0200

    Create simple example using only the drag and drop method

---
+++ Examples/DemoXCFramework/DemoXCFramework/DemoXCFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoXCFrameworkApp.swift
+//  DemoXCFramework
+//
+//  Created by Petr Palata on 03.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoXCFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
