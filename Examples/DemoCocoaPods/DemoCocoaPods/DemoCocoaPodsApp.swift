728be9c331205f1b666f7a382f2abdc1a9077401
Petr Palata <petr.palata@gmail.com>
2022-08-19 23:19:50 +0200

    Add minimal example using CocoaPods as package manager

---
+++ Examples/DemoCocoaPods/DemoCocoaPods/DemoCocoaPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCocoaPodsApp.swift
+//  DemoCocoaPods
+//
+//  Created by Petr Palata on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoCocoaPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
