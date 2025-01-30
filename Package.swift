6f37e7f7de3901feedd35e244c453d106b3f336f
Michał Gutowski <mgutski@icloud.com>
2025-01-30 13:41:05 +0100

    Bump Swift package version to 2.8.0

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let checksum = "cdceb6f13d2302bfdbf95f26d013264c678058ffbf1226034aaa0be1763d796b"
-let version = "2.7.0"
+let checksum = "59da30952f98a0497e6c07bf4e6ae067c3c4dc5f914c90e05b40db54d9cc769a"
+let version = "2.8.0"
 
 let package = Package(
     name: "FingerprintPro",
